1d17fb1be139b3902b5c91fd571391a185fa2a59
David Konsumer <konsumer@jetboystudio.com>
2023-06-20 22:25:14 -0700

    working demo

--- raylib.c
+++ raylib.c
@@ -1,5 +1,3 @@
-// docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk emcc raylib.c lib/libraylib.a -o raylib.js -I include -DPLATFORM_WEB -sASYNCIFY -sUSE_GLFW=3 -sEXPORT_ALL=1 -sLINKABLE=1
-
 #include <emscripten/emscripten.h>
 #define RLAPI EMSCRIPTEN_KEEPALIVE
 #include "raylib.h"
--- raylib_engine.js
+++ raylib_engine.js
@@ -14,6 +14,7 @@ onResize()
 class Color {
   constructor(r, g, b, a) {
     this.bytes = new Uint8Array(4)
+    this.address = Module._malloc(4)
     this.r = r
     this.g = g
     this.b = b
@@ -35,54 +36,29 @@ class Color {
   
   set r (v) {
     this.bytes[0] = v
+    Module.setValue(this.address, v)
   }
   set g (v) {
     this.bytes[1] = v
+    Module.setValue(this.address + 1, v)
   }
   set b (v) {
     this.bytes[2] = v
+    Module.setValue(this.address + 2, v)
   }
   set a (v) {
     this.bytes[3] = v
-  }
-
-  toString(){
-    return `Color( r:${this.r}, g:${this.g}, b:${this.b}, a:${this.a} )`
+    Module.setValue(this.address + 3, v)
   }
 }
 
 window.raylib = {
-  // TODO: can these be exported directly from raylib.c?
-  LIGHTGRAY: new Color(200, 200, 200, 255),
-  GRAY: new Color(130, 130, 130, 255),
-  DARKGRAY: new Color(80, 80, 80, 255),
-  YELLOW: new Color(253, 249, 0, 255),
-  GOLD: new Color(255, 203, 0, 255),
-  ORANGE: new Color(255, 161, 0, 255),
-  PINK: new Color(255, 109, 194, 255),
-  RED: new Color(230, 41, 55, 255),
-  MAROON: new Color(190, 33, 55, 255),
-  GREEN: new Color(0, 228, 48, 255),
-  LIME: new Color(0, 158, 47, 255),
-  DARKGREEN: new Color(0, 117, 44, 255),
-  SKYBLUE: new Color(102, 191, 255, 255),
-  BLUE: new Color(0, 121, 241, 255),
-  DARKBLUE: new Color(0, 82, 172, 255),
-  PURPLE: new Color(200, 122, 255, 255),
-  VIOLET: new Color(135, 60, 190, 255),
-  DARKPURPLE: new Color(112, 31, 126, 255),
-  BEIGE: new Color(211, 176, 131, 255),
-  BROWN: new Color(127, 106, 79, 255),
-  DARKBROWN: new Color(76, 63, 47, 255),
-  WHITE: new Color(255, 255, 255, 255),
-  BLACK: new Color(0, 0, 0, 255),
-  BLANK: new Color(0, 0, 0, 0),
-  MAGENTA: new Color(255, 0, 255, 255),
-  RAYWHITE: new Color(245, 245, 245, 255),
-
   // TODO: this could be auto-generated or libraryized
   ClearBackground(color) {
-    Module._ClearBackground(color.bytes)
+    Module._ClearBackground(color.address)
+  },
+  DrawRectangle(x, y, h, w, color) {
+    Module._DrawRectangle(x, y, h, w, color.address)
   }
 }
 
@@ -98,6 +74,39 @@ window.Module = {
           window.raylib[k] = window.Module[o]
         }
       }
+    }
+
+    const colors = {
+      LIGHTGRAY: new Color(200, 200, 200, 255),
+      GRAY: new Color(130, 130, 130, 255),
+      DARKGRAY: new Color(80, 80, 80, 255),
+      YELLOW: new Color(253, 249, 0, 255),
+      GOLD: new Color(255, 203, 0, 255),
+      ORANGE: new Color(255, 161, 0, 255),
+      PINK: new Color(255, 109, 194, 255),
+      RED: new Color(230, 41, 55, 255),
+      MAROON: new Color(190, 33, 55, 255),
+      GREEN: new Color(0, 228, 48, 255),
+      LIME: new Color(0, 158, 47, 255),
+      DARKGREEN: new Color(0, 117, 44, 255),
+      SKYBLUE: new Color(102, 191, 255, 255),
+      BLUE: new Color(0, 121, 241, 255),
+      DARKBLUE: new Color(0, 82, 172, 255),
+      PURPLE: new Color(200, 122, 255, 255),
+      VIOLET: new Color(135, 60, 190, 255),
+      DARKPURPLE: new Color(112, 31, 126, 255),
+      BEIGE: new Color(211, 176, 131, 255),
+      BROWN: new Color(127, 106, 79, 255),
+      DARKBROWN: new Color(76, 63, 47, 255),
+      WHITE: new Color(255, 255, 255, 255),
+      BLACK: new Color(0, 0, 0, 255),
+      BLANK: new Color(0, 0, 0, 0),
+      MAGENTA: new Color(255, 0, 255, 255),
+      RAYWHITE: new Color(245, 245, 245, 255)
+    }
+
+    for (const c of Object.keys(colors)) {
+      raylib[c] = colors[c]
     }
 
     const updateLoop = (timeStamp) => {
