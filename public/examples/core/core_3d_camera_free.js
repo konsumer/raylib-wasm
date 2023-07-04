141b77fef0aebead2b856bf111322070fbc40388
David Konsumer <konsumer@jetboystudio.com>
2023-07-04 13:09:55 -0700

    added some mor exmaples

--- public/examples/core/core_3d_camera_free.js
+++ public/examples/core/core_3d_camera_free.js
@@ -1,88 +1,53 @@
-const InitGame = async () => {
-  InitWindow(400, 250)
+let camera
+let cubePosition
+
+async function InitGame () {
+  InitWindow(800, 450)
+  
+  camera = new Camera3D()
+  camera.position.x = 10
+  camera.position.y = 10
+  camera.position.z = 10
+  camera.target.x = 0
+  camera.target.y = 0
+  camera.target.z = 0
+  camera.up.x = 0
+  camera.up.y = 1
+  camera.up.z = 0
+  camera.fovy = 45
+  camera.projection = CAMERA_PERSPECTIVE
+  
+  cubePosition = new Vector3({x: 0, y: 0, z: 0 })
+
+  DisableCursor()
 }
 
-const UpdateGame = (ts) => {
+function UpdateGame(ts) {
+  UpdateCamera(camera, CAMERA_FREE)
+  
+  if (IsKeyDown('Z')){
+    camera.target.x = 0
+    camera.target.y = 0
+    camera.target.z = 0
+  }
+
   BeginDrawing()
-  ClearBackground(BLACK)
-  DrawText("Sorry, this demo is not ported, yet.", 15, 110, 20, RAYWHITE)
+  ClearBackground(RAYWHITE)
+
+  BeginMode3D(camera)
+  DrawCube(cubePosition, 2, 2, 2, RED)
+  DrawCubeWires(cubePosition, 2, 2, 2, MAROON)
+  DrawGrid(10, 1)
+  EndMode3D()
+
+  DrawRectangle( 10, 10, 320, 133, Fade(SKYBLUE, 0.5))
+  DrawRectangleLines( 10, 10, 320, 133, BLUE)
+
+  DrawText("Free camera default controls:", 20, 20, 10, BLACK)
+  DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY)
+  DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY)
+  DrawText("- Alt + Mouse Wheel Pressed to Rotate", 40, 80, 10, DARKGRAY)
+  DrawText("- Alt + Ctrl + Mouse Wheel Pressed for Smooth Zoom", 40, 100, 10, DARKGRAY)
+  DrawText("- Z to zoom to (0, 0, 0)", 40, 120, 10, DARKGRAY)
   EndDrawing()
 }
-
-/*
-#include "raylib.h"
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
-
-    // Define the camera to look into our 3d world
-    Camera3D camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
-
-    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
-
-    DisableCursor();                    // Limit cursor to relative movement inside the window
-
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        UpdateCamera(&camera, CAMERA_FREE);
-
-        if (IsKeyDown('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            BeginMode3D(camera);
-
-                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-
-                DrawGrid(10, 1.0f);
-
-            EndMode3D();
-
-            DrawRectangle( 10, 10, 320, 133, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 320, 133, BLUE);
-
-            DrawText("Free camera default controls:", 20, 20, 10, BLACK);
-            DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
-            DrawText("- Alt + Mouse Wheel Pressed to Rotate", 40, 80, 10, DARKGRAY);
-            DrawText("- Alt + Ctrl + Mouse Wheel Pressed for Smooth Zoom", 40, 100, 10, DARKGRAY);
-            DrawText("- Z to zoom to (0, 0, 0)", 40, 120, 10, DARKGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
-}
-*/
--- public/examples/core/core_3d_picking.js
+++ public/examples/core/core_3d_picking.js
@@ -1,118 +1,89 @@
+const  screenWidth = 800
+const  screenHeight = 450
+
+let camera
+let cubePosition
+let cubeSize
+let ray
+let collision
+let bounds
+
 const InitGame = async () => {
-  InitWindow(400, 250)
+  InitWindow(screenWidth, screenHeight)
+  camera = new Camera3D()
+  camera.position.x = 10
+  camera.position.y = 10
+  camera.position.z = 10
+  camera.target.x = 0
+  camera.target.y = 0
+  camera.target.z = 0
+  camera.up.x = 0
+  camera.up.y = 1
+  camera.up.z = 0
+  camera.fovy = 45
+  camera.projection = CAMERA_PERSPECTIVE
+
+  cubePosition = new Vector3({x: 0, y: 1, z: 0 })
+  cubeSize = new Vector3({x: 2, y: 2, z: 2 })
+  ray = new Ray()
+  collision = new RayCollision()
+
+  bounds = new BoundingBox()
+  bounds.min.x = cubePosition.x - cubeSize.x/2
+  bounds.min.y = cubePosition.y - cubeSize.y/2
+  bounds.min.z = cubePosition.z - cubeSize.z/2
+  bounds.max.x = cubePosition.x + cubeSize.x/2
+  bounds.max.y = cubePosition.y + cubeSize.y/2
+  bounds.max.z = cubePosition.z + cubeSize.z/2
 }
 
 const UpdateGame = (ts) => {
+  if (IsCursorHidden()) {
+    UpdateCamera(camera, CAMERA_FIRST_PERSON)
+  }
+  
+  if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+    if (IsCursorHidden()) {
+      EnableCursor()
+    } else {
+      DisableCursor()
+    }
+  }
+
+  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+    if (!collision.hit) {
+        ray = GetMouseRay(GetMousePosition(), camera)
+        collision = GetRayCollisionBox(ray, bounds)
+    } else {
+      collision.hit = false
+    }
+  }
+
   BeginDrawing()
-  ClearBackground(BLACK)
-  DrawText("Sorry, this demo is not ported, yet.", 15, 110, 20, RAYWHITE)
+  BeginMode3D(camera);
+
+  if (collision.hit){
+    DrawCube(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, RED)
+    DrawCubeWires(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, MAROON)
+    DrawCubeWires(cubePosition, cubeSize.x + 0, cubeSize.y + 0, cubeSize.z + 0, GREEN)
+  } else {
+    DrawCube(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, GRAY)
+    DrawCubeWires(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, DARKGRAY)
+  }
+
+  DrawRay(ray, MAROON)
+  DrawGrid(10, 1)
+
+  EndMode3D()
+
+  DrawText("Try clicking on the box with your mouse!", 240, 10, 20, DARKGRAY)
+
+  if (collision.hit) {
+    DrawText("BOX SELECTED", (screenWidth - MeasureText("BOX SELECTED", 30)) / 2, (screenHeight * 0.1), 30, GREEN)
+  }
+
+  DrawText("Right click mouse to toggle camera controls", 10, 430, 10, GRAY)
+
+  DrawFPS(10, 10)
   EndDrawing()
 }
-
-/*
-#include "raylib.h"
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d picking");
-
-    // Define the camera to look into our 3d world
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
-
-    Vector3 cubePosition = { 0.0f, 1.0f, 0.0f };
-    Vector3 cubeSize = { 2.0f, 2.0f, 2.0f };
-
-    Ray ray = { 0 };                    // Picking line ray
-    RayCollision collision = { 0 };     // Ray collision hit info
-
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsCursorHidden()) UpdateCamera(&camera, CAMERA_FIRST_PERSON);
-
-        // Toggle camera controls
-        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
-        {
-            if (IsCursorHidden()) EnableCursor();
-            else DisableCursor();
-        }
-
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-        {
-            if (!collision.hit)
-            {
-                ray = GetMouseRay(GetMousePosition(), camera);
-
-                // Check collision between ray and box
-                collision = GetRayCollisionBox(ray,
-                            (BoundingBox){(Vector3){ cubePosition.x - cubeSize.x/2, cubePosition.y - cubeSize.y/2, cubePosition.z - cubeSize.z/2 },
-                                          (Vector3){ cubePosition.x + cubeSize.x/2, cubePosition.y + cubeSize.y/2, cubePosition.z + cubeSize.z/2 }});
-            }
-            else collision.hit = false;
-        }
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            BeginMode3D(camera);
-
-                if (collision.hit)
-                {
-                    DrawCube(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, RED);
-                    DrawCubeWires(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, MAROON);
-
-                    DrawCubeWires(cubePosition, cubeSize.x + 0.2f, cubeSize.y + 0.2f, cubeSize.z + 0.2f, GREEN);
-                }
-                else
-                {
-                    DrawCube(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, GRAY);
-                    DrawCubeWires(cubePosition, cubeSize.x, cubeSize.y, cubeSize.z, DARKGRAY);
-                }
-
-                DrawRay(ray, MAROON);
-                DrawGrid(10, 1.0f);
-
-            EndMode3D();
-
-            DrawText("Try clicking on the box with your mouse!", 240, 10, 20, DARKGRAY);
-
-            if (collision.hit) DrawText("BOX SELECTED", (screenWidth - MeasureText("BOX SELECTED", 30)) / 2, (int)(screenHeight * 0.1f), 30, GREEN);
-
-            DrawText("Right click mouse to toggle camera controls", 10, 430, 10, GRAY);
-
-            DrawFPS(10, 10);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
-}
-*/
