1e9ec4c4a7cda75294c11aa3ac80dc68a2762eb6
David Konsumer <konsumer@jetboystudio.com>
2024-05-12 21:19:43 -0700

    working on light shader demo

--- public/examples/shaders/shaders_basic_lighting.js
+++ public/examples/shaders/shaders_basic_lighting.js
@@ -1,148 +1,128 @@
-/*******************************************************************************************
-*
-*   raylib [shaders] example - basic lighting
-*
-*   NOTE: This example requires raylib OpenGL 3.3 or ES2 versions for shaders support,
-*         OpenGL 1.1 does not support shaders, recompile raylib to OpenGL 3.3 version.
-*
-*   NOTE: Shaders used in this example are #version 330 (OpenGL 3.3).
-*
-*   Example originally created with raylib 3.0, last time updated with raylib 4.2
-*
-*   Example contributed by Chris Camacho (@codifies) and reviewed by Ramon Santamaria (@raysan5)
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2019-2023 Chris Camacho (@codifies) and Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+const GLSL_VERSION = 100
 
-#include "raylib.h"
+const screenWidth = 800
+const screenHeight = 450
 
-#include "raymath.h"
+let camera
+let shader
+let model
+let cube
+let vec0
 
-#define RLIGHTS_IMPLEMENTATION
-#include "rlights.h"
+// eventually I might do this as a more generic uniform-handler, but this works for now
 
-#if defined(PLATFORM_DESKTOP)
-    #define GLSL_VERSION            330
-#else   // PLATFORM_RPI, PLATFORM_ANDROID, PLATFORM_WEB
-    #define GLSL_VERSION            100
-#endif
+const LIGHT_DIRECTIONAL = 0
+const LIGHT_POINT = 1
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+let lightsCount = 0
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT);  // Enable Multi Sampling Anti Aliasing 4x (if available)
-    InitWindow(screenWidth, screenHeight, "raylib [shaders] example - basic lighting");
+class Light {
+  constructor(type, position, target, color, shader){
+    this.enabledLoc = GetShaderLocation(shader, `lights[${lightsCount}].enabled`)
+    this.typeLoc = GetShaderLocation(shader, `lights[${lightsCount}].type`)
+    this.positionLoc = GetShaderLocation(shader, `lights[${lightsCount}].position`)
+    this.targetLoc = GetShaderLocation(shader, `lights[${lightsCount}].target`)
+    this.colorLoc = GetShaderLocation(shader, `lights[${lightsCount}].color`)
+    this.shader = shader
+    this.type = type
+    this.position = position
+    this.target = target
+    this.color = color
+    this.enabled = true
 
-    // Define the camera to look into our 3d world
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 2.0f, 4.0f, 6.0f };    // Camera position
-    camera.target = (Vector3){ 0.0f, 0.5f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+    lightsCount++
+  }
 
-    // Load plane model from a generated mesh
-    Model model = LoadModelFromMesh(GenMeshPlane(10.0f, 10.0f, 3, 3));
-    Model cube = LoadModelFromMesh(GenMeshCube(2.0f, 4.0f, 2.0f));
-    
-    // Load basic lighting shader
-    Shader shader = LoadShader(TextFormat("resources/shaders/glsl%i/lighting.vs", GLSL_VERSION),
-                               TextFormat("resources/shaders/glsl%i/lighting.fs", GLSL_VERSION));
-    // Get some required shader locations
-    shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
-    // NOTE: "matModel" location name is automatically assigned on shader loading, 
-    // no need to get the location again if using that uniform name
-    //shader.locs[SHADER_LOC_MATRIX_MODEL] = GetShaderLocation(shader, "matModel");
-    
-    // Ambient light level (some basic lighting)
-    int ambientLoc = GetShaderLocation(shader, "ambient");
-    SetShaderValue(shader, ambientLoc, (float[4]){ 0.1f, 0.1f, 0.1f, 1.0f }, SHADER_UNIFORM_VEC4);
+  set enabled (val) {
+    mod.setValue(this.typeLoc, val ? 1 : 0, 'i32')
+    SetShaderValue(this.shader, this.enabledLoc, this.enabledLoc, SHADER_UNIFORM_INT)
+  }
 
-    // Assign out lighting shader to model
-    model.materials[0].shader = shader;
-    cube.materials[0].shader = shader;
+  get enabled() {
+    return !!mod.getValue(this.enabledLoc, 'i32')
+  }
 
-    // Create lights
-    Light lights[MAX_LIGHTS] = { 0 };
-    lights[0] = CreateLight(LIGHT_POINT, (Vector3){ -2, 1, -2 }, Vector3Zero(), YELLOW, shader);
-    lights[1] = CreateLight(LIGHT_POINT, (Vector3){ 2, 1, 2 }, Vector3Zero(), RED, shader);
-    lights[2] = CreateLight(LIGHT_POINT, (Vector3){ -2, 1, 2 }, Vector3Zero(), GREEN, shader);
-    lights[3] = CreateLight(LIGHT_POINT, (Vector3){ 2, 1, -2 }, Vector3Zero(), BLUE, shader);
+  set type (val) {
+    mod.setValue(this.typeLoc, val, 'i32')
+    SetShaderValue(this.shader, this.typeLoc,this.typeLoc, SHADER_UNIFORM_INT)
+  }
 
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+  get type() {
+    return mod.getValue(this.typeLoc, 'i32')
+  }
 
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        UpdateCamera(&camera, CAMERA_ORBITAL);
+  set position (val) {
+    SetShaderValue(this.shader, this.positionLoc, val._address, SHADER_UNIFORM_INT)
+  }
 
-        // Update the shader with the camera view vector (points towards { 0.0f, 0.0f, 0.0f })
-        float cameraPos[3] = { camera.position.x, camera.position.y, camera.position.z };
-        SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], cameraPos, SHADER_UNIFORM_VEC3);
-        
-        // Check key inputs to enable/disable lights
-        if (IsKeyPressed(KEY_Y)) { lights[0].enabled = !lights[0].enabled; }
-        if (IsKeyPressed(KEY_R)) { lights[1].enabled = !lights[1].enabled; }
-        if (IsKeyPressed(KEY_G)) { lights[2].enabled = !lights[2].enabled; }
-        if (IsKeyPressed(KEY_B)) { lights[3].enabled = !lights[3].enabled; }
-        
-        // Update light values (actually, only enable/disable them)
-        for (int i = 0; i < MAX_LIGHTS; i++) UpdateLightValues(shader, lights[i]);
-        //----------------------------------------------------------------------------------
+  get position() {
+    return new Vector3({}, this.positionLoc)
+  }
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+  set target (val) {
+    SetShaderValue(this.shader, this.targetLoc, val._address, SHADER_UNIFORM_INT)
+  }
 
-            ClearBackground(RAYWHITE);
+  get target() {
+    return new Vector3({}, this.targetLoc)
+  }
 
-            BeginMode3D(camera);
+  set color (val) {
+    SetShaderValue(this.shader, this.colorLoc, val._address, SHADER_UNIFORM_INT)
+  }
 
-                DrawModel(model, Vector3Zero(), 1.0f, WHITE);
-                DrawModel(cube, Vector3Zero(), 1.0f, WHITE);
-
-                // Draw spheres to show where the lights are
-                for (int i = 0; i < MAX_LIGHTS; i++)
-                {
-                    if (lights[i].enabled) DrawSphereEx(lights[i].position, 0.2f, 8, 8, lights[i].color);
-                    else DrawSphereWires(lights[i].position, 0.2f, 8, 8, ColorAlpha(lights[i].color, 0.3f));
-                }
-
-                DrawGrid(10, 1.0f);
-
-            EndMode3D();
-
-            DrawFPS(10, 10);
-
-            DrawText("Use keys [Y][R][G][B] to toggle lights", 10, 40, 20, DARKGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadModel(model);     // Unload the model
-    UnloadModel(cube);      // Unload the model
-    UnloadShader(shader);   // Unload shader
-
-    CloseWindow();          // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
+  get color() {
+    return new Color({}, this.colorLoc)
+  }
 }
 
+
+
+const InitGame = async () => {
+  SetConfigFlags(FLAG_MSAA_4X_HINT)
+  InitWindow(screenWidth, screenHeight, "raylib [shaders] example - basic lighting")
+  camera = new Camera()
+  vec0 = Vector3Zero()
+  
+  camera.position.x = 2
+  camera.position.y = 4
+  camera.position.z = 6
+
+  camera.target.x = 0
+  camera.target.y = 0.5
+  camera.target.z = 0
+
+  camera.up.x = 0
+  camera.up.y = 1
+  camera.up.z = 0
+
+  camera.fovy = 45
+  camera.projection = CAMERA_PERSPECTIVE
+
+  await addFile(`/examples/shaders/resources/shaders/glsl${GLSL_VERSION}/lighting.vs`, 'lighting.vs')
+  await addFile(`/examples/shaders/resources/shaders/glsl${GLSL_VERSION}/lighting.fs`, 'lighting.fs')
+  shader = await LoadShader('lighting.vs', 'lighting.fs')
+
+  model = LoadModelFromMesh(GenMeshPlane(10.0, 10.0, 3, 3))
+  cube = LoadModelFromMesh(GenMeshCube(2.0, 4.0, 2.0))
+
+  const lights = [
+    new Light(LIGHT_POINT, new Vector3({x: -2, y: 1, z: -2 }), vec0, YELLOW, shader)
+  ]
+}
+
+const UpdateGame = (ts) => {
+  BeginDrawing()
+  ClearBackground(RAYWHITE)
+  BeginMode3D(camera)
+  DrawModel(model, vec0, 1, WHITE)
+  DrawModel(cube, vec0, 1, WHITE)
+
+  // TODO: loop over lights
+
+  DrawGrid(10, 1)
+  EndMode3D()
+  DrawFPS(10, 10)
+  DrawText("Use keys [Y][R][G][B] to toggle lights", 10, 40, 20, DARKGRAY)
+  EndDrawing()
+}
