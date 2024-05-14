66d8bd72f3dae033f1b6724bfe55af9bec58537a
David Konsumer <konsumer@jetboystudio.com>
2024-05-14 09:59:07 -0700

    setup more files in publish

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ export default {
   build: {
     rollupOptions: {
       input: {
-        'demo': "src/demo.js",
-        'raylib': "src/raylib.js",
-        'index.html': "index.html",
+        demo: 'src/demo.js',
+        raylib: 'src/raylib.js',
+        'raylib-wc': 'src/raylib-wc.js',
+        raylib_emscripten: 'src/raylib_emscripten.js',
+        'index.html': 'index.html'
       },
       output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
       }
     }
   }
