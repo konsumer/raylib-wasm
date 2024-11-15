69c7c71775b8f4f6ed2dc69f6cf323f72e941ba7
David Konsumer <konsumer@jetboystudio.com>
2024-11-14 22:23:36 -0800

    non-wokrking audio demo

--- public/examples/audio/audio_sound_loading.js
+++ public/examples/audio/audio_sound_loading.js
@@ -1,69 +1,25 @@
+let fxWav
+let fxOgg
+
 const InitGame = async () => {
-  InitWindow(400, 250)
+  InitWindow(800, 450)
+  InitAudioDevice()
+  fxWav = LoadSound('resources/sound.wav')
+  fxOgg = LoadSound('resources/target.ogg')
 }
 
 const UpdateGame = (ts) => {
   BeginDrawing()
-  ClearBackground(BLACK)
-  DrawText("Sorry, this demo is not ported, yet.", 15, 110, 20, RAYWHITE)
+  ClearBackground(RAYWHITE)
+  DrawText('Press SPACE to PLAY the WAV sound!', 200, 180, 20, LIGHTGRAY)
+  DrawText('Press ENTER to PLAY the OGG sound!', 200, 220, 20, LIGHTGRAY)
   EndDrawing()
+  if (IsKeyPressed(KEY_SPACE)) {
+    PlaySound(fxWav)
+    console.log('space')
+  }
+  if (IsKeyPressed(KEY_ENTER)) {
+    PlaySound(fxOgg)
+    console.log('enter')
+  }
 }
-
-/*
-#include "raylib.h"
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [audio] example - sound loading and playing");
-
-    InitAudioDevice();      // Initialize audio device
-
-    Sound fxWav = LoadSound("resources/sound.wav");         // Load WAV audio file
-    Sound fxOgg = LoadSound("resources/target.ogg");        // Load OGG audio file
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyPressed(KEY_SPACE)) PlaySound(fxWav);      // Play WAV sound
-        if (IsKeyPressed(KEY_ENTER)) PlaySound(fxOgg);      // Play OGG sound
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Press SPACE to PLAY the WAV sound!", 200, 180, 20, LIGHTGRAY);
-            DrawText("Press ENTER to PLAY the OGG sound!", 200, 220, 20, LIGHTGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadSound(fxWav);     // Unload sound data
-    UnloadSound(fxOgg);     // Unload sound data
-
-    CloseAudioDevice();     // Close audio device
-
-    CloseWindow();          // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
-}
-*/
