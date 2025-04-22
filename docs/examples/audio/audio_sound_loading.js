09936b5f360b9c881c920bf98c4b8eec0af32823
David Konsumer <konsumer@jetboystudio.com>
2025-04-21 17:06:59 -0700

    put keys before drawing

--- docs/examples/audio/audio_sound_loading.js
+++ docs/examples/audio/audio_sound_loading.js
@@ -6,22 +6,18 @@ const InitGame = async () => {
   InitAudioDevice()
   fxWav = await LoadSound('examples/audio/resources/sound.wav')
   fxOgg = await LoadSound('examples/audio/resources/target.ogg')
-  // say hi
-  PlaySound(fxWav)
 }
 
 const UpdateGame = (ts) => {
+  if (IsKeyPressed(KEY_SPACE)) {
+    PlaySound(fxWav)
+  }
+  if (IsKeyPressed(KEY_ENTER)) {
+    PlaySound(fxOgg)
+  }
   BeginDrawing()
   ClearBackground(RAYWHITE)
   DrawText('Press SPACE to PLAY the WAV sound!', 200, 180, 20, LIGHTGRAY)
   DrawText('Press ENTER to PLAY the OGG sound!', 200, 220, 20, LIGHTGRAY)
   EndDrawing()
-  if (IsKeyPressed(KEY_SPACE)) {
-    PlaySound(fxWav)
-    console.log('space')
-  }
-  if (IsKeyDown(KEY_ENTER)) {
-    PlaySound(fxOgg)
-    console.log('enter')
-  }
 }
